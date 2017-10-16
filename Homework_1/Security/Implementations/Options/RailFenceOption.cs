22af3fd2836ba84ffd274cef233259628f553139
tcconstantin <31178157+tcconstantin@users.noreply.github.com>
2017-10-16 20:26:35 +0300

    add header for all files

--- Homework_1/Security/Implementations/Options/RailFenceOption.cs
+++ Homework_1/Security/Implementations/Options/RailFenceOption.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        RailFenceOption.cs                                       *
+ *  Copyright:   (c) 2017, Todireanu Constantin Catalin                   *
+ *  Description: MasterSecurity-Homework_1                                *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
--- Homework_1/Security/Interfaces/ICrypto.cs
+++ Homework_1/Security/Interfaces/ICrypto.cs
@@ -1,4 +1,18 @@
-﻿using Security.Utils.FileHelper;
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        ICrypto.cs                                               *
+ *  Copyright:   (c) 2017, Todireanu Constantin Catalin                   *
+ *  Description: MasterSecurity-Homework_1                                *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
+using Security.Utils.FileHelper;
 using System;
 using System.Collections.Generic;
 using System.Text;
